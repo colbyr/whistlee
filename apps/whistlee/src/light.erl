d5c3be0ed1964dab1e7707f97404cb34de02abc8
Colby Rabideau <me@colbyr.com>
2021-08-17 14:53:56 -0400

    implement gen_server

--- apps/whistlee/src/light.erl
+++ apps/whistlee/src/light.erl
@@ -1,41 +1,72 @@
 -module(light).
+-behavior(gen_server).
 -export([
-  light/1,
+  init/1,
+  handle_call/3,
+  handle_cast/2,
+  handle_info/2,
   start/1,
+  start_link/1,
+  code_change/3,
+  terminate/2,
+  disconnect/1,
   turn_off/1,
   turn_on/1
 ]).
 
-light(LightId) ->
-  receive
-    {From, {on}} ->
-      Resp = hue:light_on(LightId),
-      From ! {self(), {ok, Resp}},
-      light(LightId);
+init(LightId) ->
+  io:format("Conntecting to light #~p~n", [LightId]),
+  {ok, LightId}.
 
-    {From, {off}} ->
-      Resp = hue:light_off(LightId),
-      From ! {self(), {ok, Resp}},
-      light(LightId);
 
-    terminate ->
-      ok;
+code_change(_OldVsn, State, _Extra) ->
+  %% No change planned. The function is there for the behaviour,
+  %% but will not be used. Only a version on the next
+  {ok, State}.
 
-    Unexpected ->
-      io:format("unexpected message ~p~n", [Unexpected])
-  end.
 
-start(LightId) ->
-  spawn(?MODULE, light, [LightId]).
+handle_call({on}, _From, LightId) ->
+  io:format("Turn ON light #~p~n", [LightId]),
+  Resp = hue:light_on(LightId),
+  {reply, Resp, LightId};
+
+handle_call({off}, _From, LightId) ->
+  io:format("Turn OFF light #~p~n", [LightId]),
+  Resp = hue:light_off(LightId),
+  {reply, Resp, LightId};
+
+handle_call(terminate, _From, LightId) ->
+  io:format("Disconnecting from light #~p~n", [LightId]),
+  {stop, normal, ok, LightId}.
+
+
+handle_cast(Unexpected, LightId) ->
+  io:format("unexpected message ~p~n", [Unexpected]),
+  {noreply, LightId}.
+
+
+handle_info(Msg, LightId) ->
+  io:format("unexpected message ~p~n", [Msg]),
+  {noreply, LightId}.
+
+
+terminate(normal, LightId) ->
+  io:format("Light #~p was disconnected.~n",[LightId]),
+  ok.
+
+
+% Methods
+
+start(LightId) -> gen_server:start(?MODULE, LightId).
+
+start_link(LightId) -> gen_server:start_link(?MODULE, LightId, []).
 
 turn_off(Pid) ->
-  Pid ! {self(), {off}},
-  receive
-    {Pid, Msg} -> Msg
-  end.
+  gen_server:call(Pid, {off}).
 
 turn_on(Pid) ->
-  Pid ! {self(), {on}},
-  receive
-    {Pid, Msg} -> Msg
-  end.
+  gen_server:call(Pid, {on}).
+
+disconnect(Pid) ->
+  gen_server:call(Pid, terminate).
+
