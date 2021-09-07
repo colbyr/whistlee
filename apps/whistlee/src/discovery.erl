1bff410a8884dbaa7e954d9bf0300620b74327d2
Colby Rabideau <478109+colbyr@users.noreply.github.com>
2021-09-07 19:19:36 -0400

    publish the service discovery entry

--- apps/whistlee/src/discovery.erl
+++ apps/whistlee/src/discovery.erl
@@ -10,6 +10,46 @@
   terminate/2
 ]).
 
+configuration_number() ->
+  1.
+
+% based on https://github.com/homebridge/HAP-NodeJS/blob/81652da554137d818d049f6f245e88efec43b1c5/src/lib/Accessory.ts#L85-L125
+category(Category) ->
+  case Category of
+    other -> 1;
+    switch -> 8;
+    _ -> category(other)
+  end.
+
+% based on https://github.com/homebridge/HAP-NodeJS/blob/81652da554137d818d049f6f245e88efec43b1c5/src/lib/Advertiser.ts#L27-L34
+pairing_feature_flag(Feature) ->
+  case Feature of
+    supports_hardware_authentication -> 16#01;
+    supports_software_authentication -> 16#02
+  end.
+
+% based on https://github.com/homebridge/HAP-NodeJS/blob/81652da554137d818d049f6f245e88efec43b1c5/src/lib/Advertiser.ts#L17-L25
+status_flag(Status) ->
+  case Status of
+    not_paired -> 16#01;
+    not_joined_wifi -> 16#02;
+    problem_detected -> 16#04
+  end.
+
+% based on https://github.com/homebridge/HAP-NodeJS/blob/81652da554137d818d049f6f245e88efec43b1c5/src/lib/Advertiser.ts#L160-L164
+setup_hash(DeviceId, SetupId) ->
+  Hash = crypto:hash(sha512, [DeviceId, SetupId]),
+  << Prefix:4/binary, _/binary >> = Hash,
+  base64:encode_to_string(Prefix).
+
+format_txt_value(Int) when is_integer(Int) ->
+  io_lib:format("~p", [Int]);
+format_txt_value(Value) ->
+  Value.
+
+format_txt(Txt) ->
+  [{Key, format_txt_value(Value)} || {Key, Value} <- Txt].
+
 log(Msg, Args) ->
   io:format("discovery: " ++ Msg ++ "~n", Args).
 log (Msg) ->
@@ -20,7 +60,22 @@ start_link(Name, Type, Port) ->
 
 init({Name, Type, Port}) ->
   log("registering"),
-  {ok, Ref} = dnssd:register(Name, Type, Port),
+  DeviceId = "C3:5D:3A:AE:5E:FA",
+  Txt = [
+    % based on https://github.com/homebridge/HAP-NodeJS/blob/master/src/lib/Advertiser.ts#L147-L157
+    {'c#', configuration_number()}, % current configuration number
+    {ff, pairing_feature_flag(supports_software_authentication)}, % pairing feature flags
+    {id, DeviceId}, % device id
+    {md, "whistlee@0.1.0"}, % model name
+    {pv, "1.1"}, % protocol version
+    {'s#', "1"}, % current state number (must be 1)
+    {sf, status_flag(not_paired)}, % status flag
+    {ci, category(switch)}, % accessory category
+    {sh, setup_hash(DeviceId, "")} % setup hash
+  ],
+  FormattedTxt = format_txt(Txt),
+  log("registering~p", [FormattedTxt]),
+  {ok, Ref} = dnssd:register(Name, Type, Port, FormattedTxt),
   {ok, {Ref}}.
 
 handle_call(_E, _From, State) ->
